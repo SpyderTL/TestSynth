0c54f99c4b970efbb06af3b9a56229b591df2ea6
SpyderTL <j_bair@bellsouth.net>
2018-06-30 18:15:54 -0500

    Working on Midi In.

--- TestSynth/Program.cs
+++ TestSynth/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Media;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,9 +17,24 @@ namespace TestSynth
 	class Program
 	{
 		static AutoResetEvent BufferEnd;
+		static bool[] MidiNotes = new bool[128];
 
 		static void Main(string[] args)
 		{
+			var devices = Midi.midiInGetNumDevs();
+			var deviceHandle = IntPtr.Zero;
+			var deviceCaps = new Midi.MidiInCaps();
+
+			for (var device = 0U; device < devices; device++)
+			{
+				Midi.midiInOpen(out deviceHandle, device, MidiProc, IntPtr.Zero, Midi.CALLBACK_FUNCTION);
+				Midi.midiInGetDevCaps(deviceHandle, ref deviceCaps, (uint)Marshal.SizeOf(deviceCaps));
+
+				Console.WriteLine(deviceCaps.name);
+
+				Midi.midiInStart(deviceHandle);
+			}
+
 			var input = new DirectInput();
 
 			var keyboard = new Keyboard(input);
@@ -68,7 +84,7 @@ namespace TestSynth
 					var value = 0d;
 					var count = 0;
 
-					for (int key = 0; key < Keys.Length; key++)
+					for (var key = 0; key < Keys.Length; key++)
 					{
 						if (keyboardState.IsPressed(Keys[key]))
 						{
@@ -77,6 +93,18 @@ namespace TestSynth
 							//value += Waves.Triangle(time, Notes[KeyNotes[key]] * KeyOctaves[key], 0.0);
 							//value += Waves.Sawtooth(time, Notes[KeyNotes[key]] * KeyOctaves[key], 0.0);
 							count++;
+						}
+					}
+
+					for (var octave = 0; octave < 8; octave++)
+					{
+						for (var note = 0; note < 12; note++)
+						{
+							if (MidiNotes[24 + (octave * 12) + note])
+							{
+								value += Waves.Sine(time, Notes[note] * MidiOctaves[octave], 0.0);
+								count++;
+							}
 						}
 					}
 
@@ -96,6 +124,37 @@ namespace TestSynth
 
 				if (index == buffers.Length)
 					index = 0;
+			}
+		}
+
+		private static void MidiProc(IntPtr handle, uint msg, IntPtr instance, IntPtr param1, IntPtr param2)
+		{
+			if (msg == Midi.MIM_DATA)
+			{
+				var timestamp = param2.ToInt32();
+
+				var data = param1.ToInt32();
+
+				var status = data & 0xff;
+				var note = (data >> 8) & 0xff;
+				var velocity = (data >> 16) & 0xff;
+				var unused = data >> 24;
+
+				var message = status >> 4;
+				var channel = status & 0xf;
+
+				if (message == 0x8)
+				{
+					MidiNotes[note] = false;
+
+					Console.WriteLine("Note Off: " + note + " Velocity: " + velocity + " Channel: " + channel);
+				}
+				else if (message == 0x9)
+				{
+					MidiNotes[note] = true;
+
+					Console.WriteLine("Note On: " + note + " Velocity: " + velocity + " Channel: " + channel);
+				}
 			}
 		}
 
@@ -247,6 +306,18 @@ namespace TestSynth
 			1.0,
 			1.0,
 			1.0,
+		};
+
+		private static readonly double[] MidiOctaves = new double[]
+		{
+			0.125,
+			0.25,
+			0.5,
+			1.0,
+			2.0,
+			4.0,
+			8.0,
+			16.0
 		};
 	}
 }
